7e98d1a56edb7f1466252d3f516d019e27cdd19c
Taher Jalal <147136539+jalaltaher@users.noreply.github.com>
2025-05-17 22:04:46 +0200

    Add files via upload

--- fit.sh
+++ fit.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Inputs : train_starts train_ends dataset_name timezone
-Rscript R/fit.R 'config_domestics'
-Rscript R/fit.R 'config_sap'
-Rscript R/fit.R 'config_palo_alto'
+#Rscript R/fit.R 'config_domestics'
+#Rscript R/fit.R 'config_sap'
+#Rscript R/fit.R 'config_palo_alto'
+
+# Fit with added Park.Duration
+#Rscript R_modified/fit_modified.R 'config_palo_alto'
+Rscript R_modified/fit_modified.R 'config_sap'
+
+# Fit domestics with added Park.Duration
+#Rscript R_modified/fit_modified.R 'config_domestics_puissance3'
+#Rscript R_modified/fit_modified.R 'config_domestics_puissance6'
+#Rscript R_modified/fit_modified.R 'config_domestics_puissance10'
+
--- simulate.sh
+++ simulate.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 # Inputs : model/data factor horizon
-# Rscript R/simulate.R 'config_domestics'
-Rscript R/simulate.R 'config_sap'
-# Rscript R/simulate.R 'config_palo_alto'
+#Rscript R/simulate.R 'config_domestics'
+#Rscript R/simulate.R 'config_sap'
+#Rscript R/simulate.R 'config_palo_alto'
+#Rscript R_modified/simulate_modified.R 'config_palo_alto'
+Rscript R_modified/simulate_modified.R 'config_sap'
+#Rscript R_modified/simulate_modified.R 'config_domestics'
+
+
+# Simulation domestics with added Parking Duration
+#Rscript R_modified/simulate_modified.R 'config_domestics_puissance3'
+#Rscript R_modified/simulate_modified.R 'config_domestics_puissance6'
+#Rscript R_modified/simulate_modified.R 'config_domestics_puissance10'
+
